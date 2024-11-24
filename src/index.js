eb734f762158221854c1a1fdc85a41372ad09a87
Ruta-B <baumaneruta38@gmail.com>
2024-11-24 15:55:49 +0100

    import api

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-function generateChristmasRecipe(event) {
-    event.preventDefault();
-   
-    new Typewriter("#written-recipe", {
-        strings: "Ingredients",
-  
-        autoStart: true,
-        delay: 0.05,
-        cursor: "",
-    });
+function writeRecipe(response) {
+  console.log(response);
+  new Typewriter("#written-recipe", {
+    strings: response.data.answer,
 
+    autoStart: true,
+    delay: 0.05,
+    cursor: "",
+  });
+}
+function generateChristmasRecipe(event) {
+  event.preventDefault();
+
+  let userInputElement = document.querySelector("#user-search-input");
+  let apiKey = "a7oe402d391da40bfcfe02337et07b50";
+  let context =
+    "You write various christmas recipes in basic html. Follow user instructions. DO NOT include html word in the recipe";
+  let prompt = `user instructions: christmas recipe with ${userInputElement}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(writeRecipe);
 }
 
-
-
 let formElement = document.querySelector("#recipe");
-formElement.addEventListener("submit", generateChristmasRecipe)
+formElement.addEventListener("submit", generateChristmasRecipe);
